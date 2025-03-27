2cbf3f8cb6d541a49f63fb38213695d3ed8e2761
Lionel_Beans1111 <muladi.lionel@gmail.com>
2025-03-27 17:55:10 +0200

    it works

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*c23sxyvwlf(^@c5#qdp=r9^d7k0qtlitv=vj=v&8+a3_l7rh#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 
+DEBUG = False
 
 ALLOWED_HOSTS = ["127.0.0.1", "localhost", "13.60.162.35", "EC2"]
 
